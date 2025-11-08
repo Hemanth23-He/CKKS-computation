f8ee0d22844b408a0c771adcb5befe8d51aa7d63
Hemanth23-He <goganaboinahemanth@gmail.com>
2025-11-09 03:56:44 +0530

    Refactor CKKS key generation methods for clarity

--- ckks/ckks_key_generator.py
+++ ckks/ckks_key_generator.py
@@ -5,31 +5,27 @@ from util.public_key import PublicKey
 from util.rotation_key import RotationKey
 from util.secret_key import SecretKey
 from util.random_sample import sample_triangle, sample_uniform, sample_hamming_weight_vector
+from util.barrette import BarrettReducer  # Use MontgomeryReducer if you prefer
 
 class CKKSKeyGenerator:
-
     """An instance to generate a public/secret key pair and relinearization keys.
-
     The secret key s is generated randomly, and the public key is the
     pair (-as + e, a). The relinearization keys are generated, as
     specified in the CKKS paper.
-
     Attributes:
         params (Parameters): Parameters including polynomial degree, plaintext,
-            and ciphertext modulus.
+        and ciphertext modulus.
         secret_key (Polynomial): secret key randomly generated from R_q.
         public_key (tuple of Polynomials): public key generated from
-            secret key.
+        secret key.
         relin_key (tuple of Polynomials): relinearization key generated
-            from secret key.
+        from secret key.
     """
-
     def __init__(self, params):
         """Generates secret/public key pair for CKKS scheme.
-
         Args:
             params (Parameters): Parameters including polynomial degree,
-                plaintext, and ciphertext modulus.
+            plaintext, and ciphertext modulus.
         """
         self.params = params
         self.generate_secret_key(params)
@@ -38,25 +34,25 @@ class CKKSKeyGenerator:
 
     def generate_secret_key(self, params):
         """Generates a secret key for CKKS scheme.
-
         Args:
             params (Parameters): Parameters including polynomial degree,
-                plaintext, and ciphertext modulus.
+            plaintext, and ciphertext modulus.
         """
         key = sample_hamming_weight_vector(params.poly_degree, params.hamming_weight)
         self.secret_key = SecretKey(Polynomial(params.poly_degree, key))
 
     def generate_public_key(self, params):
         """Generates a public key for CKKS scheme.
-
         Args:
             params (Parameters): Parameters including polynomial degree,
-                plaintext, and ciphertext modulus.
+            plaintext, and ciphertext modulus.
         """
         mod = self.params.big_modulus
-
+        # a
         pk_coeff = Polynomial(params.poly_degree, sample_uniform(0, mod, params.poly_degree))
+        # e
         pk_error = Polynomial(params.poly_degree, sample_triangle(params.poly_degree))
+        # -as + e
         p0 = pk_coeff.multiply(self.secret_key.s, mod)
         p0 = p0.scalar_multiply(-1, mod)
         p0 = p0.add(pk_error, mod)
@@ -65,21 +61,16 @@ class CKKSKeyGenerator:
 
     def generate_switching_key(self, new_key):
         """Generates a switching key for CKKS scheme.
-
         Generates a switching key as described in KSGen in the CKKS paper.
-
         Args:
             new_key (Polynomial): New key to generate switching key.
-
         Returns:
             A switching key.
         """
         mod = self.params.big_modulus
         mod_squared = mod ** 2
-
         swk_coeff = Polynomial(self.params.poly_degree, sample_uniform(0, mod_squared, self.params.poly_degree))
         swk_error = Polynomial(self.params.poly_degree, sample_triangle(self.params.poly_degree))
-
         sw0 = swk_coeff.multiply(self.secret_key.s, mod_squared)
         sw0 = sw0.scalar_multiply(-1, mod_squared)
         sw0 = sw0.add(swk_error, mod_squared)
@@ -90,24 +81,20 @@ class CKKSKeyGenerator:
 
     def generate_relin_key(self, params):
         """Generates a relinearization key for CKKS scheme.
-
         Args:
             params (Parameters): Parameters including polynomial degree,
-                plaintext, and ciphertext modulus.
+            plaintext, and ciphertext modulus.
         """
         sk_squared = self.secret_key.s.multiply(self.secret_key.s, self.params.big_modulus)
         self.relin_key = self.generate_switching_key(sk_squared)
 
     def generate_rot_key(self, rotation):
         """Generates a rotation key for CKKS scheme.
-
         Args:
             rotation (int): Amount ciphertext is to be rotated by.
-
         Returns:
             A rotation key.
         """
-
         # Generate K_5^r(s).
         new_key = self.secret_key.s.rotate(rotation)
         rk = self.generate_switching_key(new_key)
@@ -115,11 +102,9 @@ class CKKSKeyGenerator:
 
     def generate_conj_key(self):
         """Generates a conjugation key for CKKS scheme.
-
         Returns:
             A conjugation key.
         """
-
         # Generate K_{-1}(s).
         new_key = self.secret_key.s.conjugate()
         return self.generate_switching_key(new_key)
